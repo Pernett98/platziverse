3db8dff1768a036f869bd222356ab17fd5f0b858
Sebastian Pernett Plaza <pernett98@gmail.com>
2018-09-02 15:42:22 -0500

    setup databse and models

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgent = require('./models/agent')
+const setupMetric = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgent(config)
+  const MetricModel = setupMetric(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+  // await sequelize.sync()
+
   const Agent = {}
   const Metric = {}
 
