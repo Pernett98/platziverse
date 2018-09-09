16005b2d3a816c27a045ad01ad4a6e14d6ed0a30
Sebastian Pernett Plaza <pernett98@gmail.com>
2018-09-09 12:16:26 -0500

    setup improved

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -7,19 +7,27 @@ const db = require('./')
 
 const prompt = inquirer.createPromptModule()
 
+function askToUser() {
+  const withoutConfirm = process.argv.find((flag) => {
+    return flag === '-y' || flag === '--yes'
+  });
+  return !withoutConfirm;
+}
+
 async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your database data, are you sure'
+  if (askToUser()) {
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database data, are you sure'
+      }
+    ])
+  
+    if (!answer.setup) {
+      return console.log('Nothing happened :D')
     }
-  ])
-
-  if (!answer.setup) {
-    return console.log('Nothing happened :D')
   }
-
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
